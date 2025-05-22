e4725be4131ccd78ce9400d364be2d8b113316da
xiaofan_yu <xiaofan_yu@intsig.net>
2025-05-23 01:15:17 +0800

    fix(sandbox): code sandbox demo

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react()],
 
-  base: "/react-fabric",
-
+  base: '/react-fabric',
+  server: {
+    allowedHosts: true,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
-});
+})
